345bf4b32799e9ba45873351af363b20e2b8eb83
vivek-48 <vivekrasadiya48@gmail.com>
2022-11-24 12:06:08 +0530

    24/11

---
+++ C/Classwork/firstlecture.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+
+{
+   printf("welcome to c \t\tprogramming.\n");
+   printf("in tops technology");
+   printf("\n time is 10.30 to 12.00");
+	printf( "\nfirst name:vivek");
+	printf("\nlast name : rasadiya");
+	printf("\nfather name: dhirajbhai");
+	printf("\nbirth date:04/08/2002");
+	
+}
---
+++ C/Classwork/firstlecture2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+
+{
+	int n1=34;
+	int n2=23;
+	
+	//printf("\nA=%d",n1);
+	//printf("\nB=%d",n2);	
+	
+	printf("\nA=%d\nB=%d",n2,n1);
+}
