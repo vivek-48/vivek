eb01272ae0950a4b7d4d75bfbd795212b05df415
vivek-48 <vivekrasadiya48@gmail.com>
2023-03-11 17:13:04 +0530

    b

---
+++ javascript/ext.js
@@ -0,0 +1,40 @@
+document.write("this is external JS <br> ");
+
+
+x1 =-3.4;
+document.write(x1);// -3.4
+document.write("<br>");
+
+// typeof(varName)
+document.write(typeof(x1)); //number
+
+document.write("<br> <br>");
+
+
+
+y1 ="-3.4 skjdhf 8979 ";
+document.write(y1); //-3.4 skjdhf 8979
+document.write("<br>");
+
+// typeof(varName)
+document.write(typeof(y1));// string
+
+document.write("<br> <br>");
+
+
+
+z1 =false;
+document.write(z1);//true
+document.write("<br>");
+
+// typeof(varName)
+document.write(typeof(z1));//boolean
+
+document.write("<br> <br>");
+
+var z2;
+document.write(z2);//undefined
+document.write("<br>");
+
+// typeof(varName)
+document.write(typeof(z2));//undefined
